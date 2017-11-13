c14f5ba306eab4a1764f6d93f57fcd9e2e4f34f9
lost273 <dnk273@gmail.com>
2017-11-13 11:29:06 +0300

    13-11-2017

---
+++ TinyShop/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace TinyShop {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters (GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ TinyShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TinyShop.Startup))]
+namespace TinyShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
