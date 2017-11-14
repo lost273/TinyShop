2de9659511b13b82bdb8448b14a6012f4586b837
lost273 <dnk273@gmail.com>
2017-11-14 16:44:41 +0300

    14-11-2017-16-44

--- TinyShop/Models/ContentModels.cs
+++ TinyShop/Models/ContentModels.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace TinyShop.Models {
-    public class ContentModels {
-        public DateTime Date;
-        public List<string> Name { get; set; }
-
+    public class Сonsumption {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Cost { get; set; }
+        public decimal Total { get {return this.Quantity * this.Cost; } }
+    }
+    public class OneDay {
+        public DateTime Date { get; set; }
+        public List<Сonsumption> Name { get; set; }
+    }
+    public class OneDayContext : DbContext {
+        public OneDayContext () : base("DefaultConnection")  { }
+        public DbSet<OneDay> OneDays { get; set; }
     }
 }
