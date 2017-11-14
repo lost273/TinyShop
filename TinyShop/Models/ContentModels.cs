d7bf77cb8b583040afe79283de46cce6127381ff
lost273 <dnk273@gmail.com>
2017-11-14 15:14:20 +0300

    14-11-2017-15-14

---
+++ TinyShop/Models/ContentModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TinyShop.Models {
+    public class ContentModels {
+        public DateTime Date;
+        public List<string> Name { get; set; }
+
+    }
+}
