b8a159a10717cd6736dd9643ed2f5d2bfa39bc6d
lost273 <dnk273@gmail.com>
2017-11-15 11:03:11 +0300

    15-11-2017-11-03

--- TinyShop/Controllers/HomeController.cs
+++ TinyShop/Controllers/HomeController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TinyShop.Filters;
+using TinyShop.Models;
 
 namespace TinyShop.Controllers {
     [MyAuth]
     public class HomeController : Controller {
+        OneDayContext db = new OneDayContext();
         public ActionResult Index () {
-            return View();
+            return View(db.Products);
         }
 
         public ActionResult Diagram () {
