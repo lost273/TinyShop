120f97e4e5d08df38575a17b0202b95480d4024e
lost273 <dnk273@gmail.com>
2017-11-20 21:00:22 +0300

    20-11-2017-21-00

---
+++ TinyShop/Scripts/multiply.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('.form-control').on('change', function () {
+        if (!isNaN(parseFloat($(this).val()))) {
+            $('#total').val((parseFloat($('#quantity').val()) * parseFloat($('#cost').val())).toFixed(2));
+        }
+    });
+});
