ab076207ff6330841cdc74ff74acfab3c1e08cc8
lost273 <dnk273@gmail.com>
2017-11-28 17:31:24 +0300

    28-11-2017-17-31

--- TinyShop/Scripts/multiplication.js
+++ TinyShop/Scripts/multiplication.js
@@ -1,9 +1,15 @@
 ﻿$(document).ready(function () {
     $('.form-control').on('change', function () {
         if (!isNaN(parseFloat($(this).val()))) {
+            //replace the comma with a period for the multiplication operation
             var quantity = parseFloat($('#quantity').val().replace(',', '.'));
             var cost = parseFloat($('#cost').val().replace(',', '.'));
+            //replace the period with a comma for the validation
             $('#total').val((quantity * cost).toFixed(2).replace('.', ','));
+        }
+        //replace dot to comma in the input element
+        if ($(this).val() == $('#cost').val()) {
+            $('#cost').val($('#cost').val().replace('.', ','));
         }
     });
 });
